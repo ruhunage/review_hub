96e5710f8368d638c3f010f136af517b8d7cc3a0
Sasindu Akalanka <137613265+Sasindu003@users.noreply.github.com>
2026-06-18 22:51:33 +0530

    test

--- Start maven.cmd
+++ Start maven.cmd
@@ -2,7 +2,7 @@ call mvn spring-boot:run
 if %ERRORLEVEL% NEQ 0 (
     echo.
     echo ---------------------------------------------------
-    echo An error occurred! Keeping the window open for you.
+    echo An error occurred! Keeping the window open for you..
     echo ---------------------------------------------------
     pause
 )
